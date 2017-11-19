529ea49a8e36e010530a09ef6bfdf96390171c29
Sannat <rz_kumr@yahoo.com>
2017-11-19 17:30:52 +0200

    Code to print the messages

---
+++ chapter2/myfirst.cpp
@@ -0,0 +1,9 @@
+//myfirst.cpp -- displays a message
+#include<iostream>   				  //a preprocessor directive
+using namespace std;  				  //make definitions visible
+int main()            			 	  //function heading
+{						  //starts function body	
+	cout<< "Come up and C++ me some time.";   //message
+	cout<< "\n"; 				  //prints line breaks
+	return 0;    				  //terminate main body
+}                                                 //End of the function body
