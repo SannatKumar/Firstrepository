abaa70fa10a90462a02828dd49dfd4a79cedffa3
Sannat <rz_kumr@yahoo.com>
2017-11-21 16:02:42 +0200

    Added the program in C++ to display the varible

---
+++ chapter2/fleas.cpp
@@ -0,0 +1,13 @@
+//fleas.cpp -- displays the value of a variable
+#include<iostream>
+using namespace std;
+int main()
+{
+	int fleas; // create an integer variable
+	
+	fleas = 38; //give a value to the variable;
+	cout<< "My cat has ";
+	cout<< fleas; //displays the value of the fleas
+	cout<< "fleas./n";
+	return 0;
+}
