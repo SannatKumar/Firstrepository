e75160c72bcefa079e9e6379859bb24678b49fc0
Sannat <rz_kumr@yahoo.com>
2017-11-23 13:46:36 +0200

    Counting cat fleas

---
+++ chapter2/yourcat.cpp
@@ -0,0 +1,13 @@
+// yourcat.cpp -- input and output
+#include<iostream>
+using namespace std;
+int main()
+{
+	int fleas;
+	cout<<"How many fleas does your cat have?\n";
+	cin>> fleas; //c++ input
+	//next line concatenations output
+	//next line concatenates output
+	cout <<"Well, that's " << fleas<<" fleas too many!\n";
+	return 0;
+}
